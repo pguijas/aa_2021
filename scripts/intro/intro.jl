eb546dd6fc7f1a225d20a25ce94849cc658b7106
Pedro Guijas Bravo <p.guijas@udc.es>
2021-02-11 19:01:20 +0100

    BDs en misma carpeta que los scripts

--- scripts/intro/intro.jl
+++ scripts/intro/intro.jl
@@ -1,7 +1,8 @@
 using DelimitedFiles;
-
 dataset = readdlm("iris.data",',');
-
-# Separamos los inputs de los outputs
+#Separamos los inputs de los outputs
 inputs = dataset[:,1:4];
 targets = dataset[:,5];
+#Debemos converertir los tipos (concretar) Any -> Float64
+inputs = convert(Array{Float64,2},inputs);
+targets = convert(Array{Float64,2},targets);
