a35a6845fd457251aa002e68d4bf2389e2dd25b5
Pedro Guijas Bravo <p.guijas@udc.es>
2021-02-11 19:26:19 +0100

    Estandarizado, o eso creo :)

--- scripts/intro/intro.jl
+++ scripts/intro/intro.jl
@@ -5,4 +5,11 @@ inputs = dataset[:,1:4];
 targets = dataset[:,5];
 #Debemos converertir los tipos (concretar) Any -> Float64
 inputs = convert(Array{Float64,2},inputs);
-targets = convert(Array{Float64,2},targets);
+#Con los targets adaptar el problema (3 categorías en este caso)
+#   Iris Virginica
+#   Iris Versicolour
+#   Iris Setosa
+targets = convert(Vector{String},targets); #supongo que hay que transformarlo previamente a string para tratarlo como tal
+# [IrisVi IrisVe IrisS] bool array
+normalizador_chulisimo(x::String) = [x=="Iris-virginica" x=="Iris-versicolor" x=="Iris-setosa"]
+targets = normalizador_chulisimo.(targets)
