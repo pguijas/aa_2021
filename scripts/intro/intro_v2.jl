6b600145031731bcd0f529356e53877b6019b29f
Eliseo Bao <eliseo.bao@udc.es>
2021-02-23 17:20:27 +0100

    Outra implementación diferente á de Pedro

---
+++ scripts/intro/intro_v2.jl
@@ -0,0 +1,56 @@
+# =============================================================================
+# Eliseo Bao Souto
+# Práctica 1 - Introdución
+# =============================================================================
+
+using DelimitedFiles
+
+# -----------------------------------------------------------------------------
+
+#=
+Función para normalizar os campos categóricos en valores booleanos.
+
+Comparamos cada elemento de x coas diferentes clases. Isto devuelve un
+vector con n elementos, donde n é o número de clases diferentes. Cada
+elemento corresponde a unha columna da matriz output.
+
+Isto ten unha salvedade. Se o número de clases é 2, neste caso só temos
+unha saída desexada por patrón, polo que a matriz ten tamaño Nx!, é dicir,
+é un vector. Por este motivo é máis práctico que en lugar de Array{Bool,2}
+sexa de tipo Array{Bool,1} (Parte true da cláusula if-else)
+=#
+function normalize(x::Array)
+
+    clases = unique(x)
+    if size(clases, 1) == 2
+        output = [convert(Array{Bool}, x .== clases[1])]
+    else
+        output = [convert(Array{Bool}, x .== i) for i in clases]
+    end
+
+    return reduce(hcat, output)
+end
+
+# -----------------------------------------------------------------------------
+
+# Cargamos a base de datos
+dataset = readdlm("iris.data",',')
+
+# Separamos os inputs dos outputs
+inputs = dataset[:,1:4]
+targets = dataset[:,5]
+
+# Programación defensiva (checkeando condición)
+@assert (size(inputs,1) == size(targets,1))
+
+# Convertimos os valores dos inputs de Any a Float64
+inputs = Float64.(inputs)
+
+# Convertimos los valores dos targets de Any a String
+targets = String.(targets)
+
+# Codificamos os valores categóricos dos targets
+targets = normalize(targets)
+
+# Programación defensiva (checkeando condición)
+@assert (size(inputs,1) == size(targets,1))
