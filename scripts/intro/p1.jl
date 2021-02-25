9fc67204c5466a216ff31cd6dad5127600af80bf
the-scripting-boy <hectorpadint00@gmail.com>
2021-02-25 16:18:15 +0100

    bucle para crear neuronas implementado.
    Ya le podemos pasar funciones de activación, lo hice como una tupla de (num neuronas capa oculta, funcion) y no como una matriz pq lo que le pasamos siempre va a ser num neuronas + func (2 elem), entonces no me hace falta una lista o array que son de dimensión variable.
    Tenía un error en el bucle, y es que pillaba el número de filas de inputs en vez de el num de columnas, entonces tenía una rna con 150 entradas en vez de 4.
    Queda que el bucle entrene a la rna.

--- scripts/intro/p1.jl
+++ scripts/intro/p1.jl
@@ -12,25 +12,28 @@ include("intro_v2.jl")
 #   criterios de parada pasados como parámetros.La función debería devolver la
 #   RNA entrenada.
 #
-# Args:
+# @args:
 #   topology: array que tiene el num de capas, neuronas/capa y funciones
 #       de activación por capa.
 #   inputs: matriz con los inputs del problema.
 #   trargets: matriz con los valores de salida deseados.
 #   stopping_cond: condiciones de parada para el entrenamiento.
 #
-function newAnn(topology::Array{Int64,1}, inputs::Array, targets::Array, stoping_cond::Any)
+# @return ann: red ya entrenada
+#
+function newAnn(topology::Array, inputs::Array, targets::Array, stoping_cond::Any)
 
     # creanos una rna
     ann = Chain();
 
     # miramos cuantas entradas tiene nuestro problema
-    numInputsLayer = size(inputs,1)
-
+    numInputsLayer = size(inputs,2)
     # el vector topology tiene el num de capas, neuronas/capa y la funcion
     # de activación de cada capa. Si no hay neuronas ocultas esta vacío.
-    for numOutputsLayer = topology
-        ann = Chain(ann..., Dense(numInputsLayer, numOutputsLayer, σ));
+    for tuple = topology
+        numOutputsLayer = tuple[1]
+        activation_function = tuple[2]
+        ann = Chain(ann..., Dense(numInputsLayer, numOutputsLayer, activation_function));
         numInputsLayer = numOutputsLayer;
     end
 
@@ -48,17 +51,28 @@ function newAnn(topology::Array{Int64,1}, inputs::Array, targets::Array, stoping
     end
 
     # entrenar la rnna
-    #loss(x, y) = Losses.crossentropy(ann(x), y);
-    #Flux.train!(loss, params(ann), [(inputs', targets')], ADAM(0.01));
 
     return ann;
 
 end
 
-# creo una rna con 2 capas ocultas, 5 neuronas en la primera y 8 en la segunda.
-ann = newAnn([5,8], inputs, targets, [])
+# función de activación reLu de teoría
+relu(x) = max(0, x)
 
+# creo una rna con 2 capas ocultas, 4 neuronas en la capa de entrada (pq en el
+# dataset de iris tenemos 4 valores para categorizar) y 5 en la primera capa
+# oculta, y 8 en la segunda capa oculta. Además de la capa de salida con 3
+# neuronas (3 clases). Además, a cada capa oculta se le pasa su función de
+# transferencia no tiene por qué ser la funcion Sigmoid, podemos usar relu o
+# cualquier otra funcion.
+ann = newAnn([(5, σ); (8, relu)], inputs, targets, [])
+# neurona sin capas ocultas
+ann2 = newAnn([], inputs, targets, [])
 
-# TO-DO list:
-#   en topology tb hay que meter funciones de activacion
-#   entrenar la rna.
+# si entrena 1 iteracción es que funciona :)
+loss(x, y) = Losses.crossentropy(ann(x), y)
+Flux.train!(loss, params(ann), [(inputs', targets')], ADAM(0.01));
+Flux.train!(loss, params(ann2), [(inputs', targets')], ADAM(0.01));
+
+# TO-DO:
+#   entrenar la rna dentro del bucle
