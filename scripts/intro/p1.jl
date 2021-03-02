d2c36bdb9c4956fefba9bce5ff9141d454c7cc11
the-scripting-boy <hectorpadint00@gmail.com>
2021-03-02 17:44:54 +0100

    Funcion de 'newAnn mejorada'.
    Definí una funcion que añada una capa a una rna externa.
    En la funcion principal hacemos load_hidden_layers.(params...) para que lo haga para todos los componentes.

--- scripts/intro/p1.jl
+++ scripts/intro/p1.jl
@@ -4,6 +4,26 @@ using Flux.Losses
 using Flux: @epochs
 include("intro_v2.jl")
 
+
+#
+#  Esta función auxiliar para añadir una capa oculta a la red neuronal
+#
+# @args:
+#   hidden_neurons: número de neuronas de la capa oculta que estamos creando.
+#   activation_function: función de activación para la capa.
+#
+function load_hidden_layers(hidden_neurons::Any, activation_function::Any)
+    # ann y numInputsLayer van a ser referenciadas fuera del bucle, entonces
+    # hace falta ponerle "global"
+    global ann, numInputsLayer
+    # añade una capa oculta a la rna que ya teníamos
+    ann = Chain(ann..., Dense(numInputsLayer, hidden_neurons, activation_function));
+    # seguimos iterando, añadido el último valor de neuronas para ponerlo en la
+    # siguiente capa y que puedan conectarse.
+    numInputsLayer = hidden_neurons;
+end
+
+
 #
 #  Esta función debe recibir la topología (número de capas y neuronas y
 #   funciones de activación en cada capa), conjunto de entrenamiento, y
@@ -22,22 +42,21 @@ include("intro_v2.jl")
 #
 # @return ann: red ya entrenada
 #
-function newAnn(topology::Array, inputs::Array, targets::Array, stoping_cond::Any)
+function newAnn(num_layers::Array, functions::Array, inputs::Array, targets::Array, stoping_cond::Any)
+    # ann y numInputsLayer van a ser referenciadas fuera del bucle, entonces
+    # hace falta ponerle "global"
+    global ann, numInputsLayer
 
-    # creanos una rna
+    # creamos una rna
     ann = Chain();
-
     # miramos cuantas entradas tiene nuestro problema
     numInputsLayer = size(inputs,2)
-    # el vector topology tiene el num de capas, neuronas/capa y la funcion
-    # de activación de cada capa. Si no hay neuronas ocultas esta vacío.
-    for tuple = topology
-        numOutputsLayer = tuple[1]
-        activation_function = tuple[2]
-        ann = Chain(ann..., Dense(numInputsLayer, numOutputsLayer, activation_function));
-        numInputsLayer = numOutputsLayer;
-    end
-
+    # si el número de capas es diferente al de neuronas de activación no podemos
+    # entrenar.
+    @assert (size(num_layers) == size(functions))
+    # le pasamos a la función el número de capas junto con sus funciones de
+    # activación
+    load_hidden_layers.(num_layers,functions)
     # miramos las clases de salida que tiene nuestro problema
     output_layers = size(targets,2)
 
@@ -45,7 +64,6 @@ function newAnn(topology::Array, inputs::Array, targets::Array, stoping_cond::An
     if (output_layers > 1)
         ann = Chain(ann..., Dense(numInputsLayer, output_layers, identity));
         ann = Chain(ann..., softmax);
-
     # si solo tenemos dos clases, usamos una rollo positivo/negativo
     else
         ann = Chain(ann..., Dense(numInputsLayer, 1, σ));
@@ -57,6 +75,7 @@ function newAnn(topology::Array, inputs::Array, targets::Array, stoping_cond::An
 
 end
 
+
 # función de activación reLu de teoría
 relu(x) = max(0, x)
 
@@ -66,14 +85,14 @@ relu(x) = max(0, x)
 # neuronas (3 clases). Además, a cada capa oculta se le pasa su función de
 # transferencia no tiene por qué ser la funcion Sigmoid, podemos usar relu o
 # cualquier otra funcion.
-ann = newAnn([(5, σ); (8, relu)], inputs, targets, [])
+ann = newAnn([5, 8], [σ, relu], inputs, targets, [])
 # neurona sin capas ocultas
-ann2 = newAnn([], inputs, targets, [])
+#ann2 = newAnn([], inputs, targets, [])
 
 # si entrena 1 iteracción es que funciona :)
 loss(x, y) = Losses.crossentropy(ann(x), y)
 Flux.train!(loss, params(ann), [(inputs', targets')], ADAM(0.01));
-Flux.train!(loss, params(ann2), [(inputs', targets')], ADAM(0.01))
+#Flux.train!(loss, params(ann2), [(inputs', targets')], ADAM(0.01))
 
 # entrena la rna 3 veces -> 3 "epochs" según los de Flux
 @epochs 3 Flux.train!(loss, params(ann), [(inputs', targets')], ADAM(0.01));
