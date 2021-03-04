1e338d29ee1d0cbaf17eb07a301e3fa9fe1d7067
the-scripting-boy <hectorpadint00@gmail.com>
2021-03-04 11:41:18 +0100

    Entrenamiento hecho.

--- scripts/intro/p1.jl
+++ scripts/intro/p1.jl
@@ -1,12 +1,10 @@
 using DelimitedFiles
-using Flux
-using Flux.Losses
-using Flux: @epochs
+using Flux: Chain, Dense, σ, softmax, onehotbatch, onecold, crossentropy, throttle, params, ADAM, train!, stop, @epochs
 include("intro_v2.jl")
 
 
 #
-#  Esta función auxiliar para añadir una capa oculta a la red neuronal
+#  Esta función auxiliar para añadir una capa oculta a la red neuronal.
 #
 # @args:
 #   hidden_neurons: número de neuronas de la capa oculta que estamos creando.
@@ -45,21 +43,29 @@ end
 #
 # @return ann: red ya entrenada
 #
-function newAnn(num_layers::Array, functions::Array, inputs::Array, targets::Array, stoping_cond::Any)
+function newAnn(topology::Array, inputs::Array, targets::Array, stoping_cond::Any)
     # ann y numInputsLayer van a ser referenciadas fuera del bucle, entonces
     # hace falta ponerle "global"
     global ann, numInputsLayer
 
     # creamos una rna
     ann = Chain();
+    # num_layers = topology[1]
+    # functions = topology[2]
     # miramos cuantas entradas tiene nuestro problema
     numInputsLayer = size(inputs,2)
     # si el número de capas es diferente al de neuronas de activación no podemos
     # entrenar.
-    @assert (size(num_layers) == size(functions))
+    # @assert (size(topology[1]) == size(topology[2]))
     # le pasamos a la función el número de capas junto con sus funciones de
     # activación
-    load_hidden_layers.(num_layers,functions)
+    # load_hidden_layers.(num_layers,functions)
+    for tuple = topology
+        numOuputsLayer = tuple[1]
+        activation_function = tuple[2]
+        ann = Chain(ann..., Dense(numInputsLayer, numOuputsLayer, activation_function));
+        numInputsLayer = numOuputsLayer;
+    end
     # miramos las clases de salida que tiene nuestro problema
     output_layers = size(targets,2)
 
@@ -73,17 +79,49 @@ function newAnn(num_layers::Array, functions::Array, inputs::Array, targets::Arr
     end
 
     # nuestra función de loss (crossentropy)
-    loss(x, y) = Losses.crossentropy(ann(x), y)
+    loss(x, y) = crossentropy(ann(x), y)
     # pesos y bías de la rna
     ps = params(ann)
+    # gradiente descentiente stochastic ADAM (optimizador)
+    opt = ADAM(0.01)
     # condiciones de parada
     iterations = stoping_conditions[1]
     max_accurracy = stoping_conditions[2]
     min_error_value = stoping_conditions[3]
-
-    # entrena la rna "iterations" veces o "iterations" epochs según los de Flux          # aquí va el valor de accurracy de la rna 
-    @epochs iterations Flux.train!(loss, ps, [(inputs', targets')], ADAM(0.01), cb = function () 0.9 > 0.9 && Flux.stop() end);
-
+    # una forma para detectar lo bien que estamos aproximando
+    # accuracy(x, y) = mean(onecold(ann(x)) .== onecold(y)) no lo podemos usar
+    evalcb = () -> @show(loss(inputs', targets'))
+    #stopping_function = function ()
+    #    println("xd")
+    #end
+    #function ()
+    #  accuracy() > 0.9 && Flux.stop()
+    #end
+    # entrena la rna "iterations" veces o "iterations" epochs según los de Flux
+    # @epochs iterations train!(loss, ps, [(inputs', targets')], opt, cb = evalcb);
+    best_ann = ann
+    best_loss = loss(inputs', targets')
+    actual_loss = loss(inputs', targets')
+    while (true)
+        mod_err = actual_loss
+        train!(loss, ps, [(inputs', targets')], opt, cb = evalcb);
+        actual_loss = loss(inputs', targets')
+        if (best_loss > actual_loss)
+            best_loss = actual_loss
+            best_ann = ann
+        end
+        mod_err -= actual_loss
+        @show(actual_loss)
+        @show(iterations)
+        @show(mod_err)
+        iterations-=1
+        if ((iterations < 0) || (actual_loss < max_accurracy) || (mod_err < min_error_value))
+            break
+        end
+    end
+    ann = best_ann
+    @show(best_loss)
+    @show(loss(inputs', targets'))
     return ann;
 
 end
@@ -101,7 +139,7 @@ stoping_conditions = [20, 0.9, 0]
 # neuronas (3 clases). Además, a cada capa oculta se le pasa su función de
 # transferencia no tiene por qué ser la funcion Sigmoid, podemos usar relu o
 # cualquier otra funcion.
-ann = newAnn([5, 8], [σ, relu], inputs, targets, stoping_conditions)
+ann = newAnn([(5, σ); (8, relu)], inputs, targets, stoping_conditions)
 # neurona sin capas ocultas
 #ann2 = newAnn([], inputs, targets, [])
 
