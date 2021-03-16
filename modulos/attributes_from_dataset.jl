8cac4e6b47de6f73478ca318a521fe6e08e6bf5d
Pedro Guijas Bravo <p.guijas@udc.es>
2021-03-16 12:47:53 +0100

    Añadida funcion que guarda el dataset

--- modulos/attributes_from_dataset.jl
+++ modulos/attributes_from_dataset.jl
@@ -27,10 +27,16 @@ function loadFolderImages(folderName::String)
 
     # Para cada fichero que detecte en la carpeta dada
     for fileName in readdir(folderName)
+        println(fileName)
         if isImageExtension(fileName)
             # Leemos la imagen
             image = load(string(folderName, "/", fileName));
             # Comprobar que el archivo cargado sea una imagen en color
+            @show (typeof(image))
+            #Rgbx Vete a tomar por culo
+            if (typeof(image)==Array{RGBX{Normed{UInt8,8}},2})
+                image=convert(Array{RGB{Normed{UInt8,8}},2},image)
+            end
             @assert(isa(image, Array{RGBA{Normed{UInt8,8}},2}) || isa(image, Array{RGB{Normed{UInt8,8}},2}))
             # Añadimos la foto al array de imagenes
             push!(images, image);
@@ -85,6 +91,7 @@ function getInputs(path)
         trues(size(positiveDataset,1));
         falses(size(negativeDataset,1))
     ];
+    targets=convert(Array{Bool, 1},targets)
 
     # Generamos la primera parte de la matriz de inputs con los elementos
     # que son positivos
@@ -99,10 +106,27 @@ function getInputs(path)
         foto = negativeDataset[rows-i + 1];
         inputs[i,:] = getAttributesFromImage(foto);
     end;
-
+    @show (typeof(targets))
     return (inputs,targets)
+end
+
+function write_dataset(file_name::String,inputs::Array{Float64, 2},targets::Array{Bool, 1})
+    f = open(file_name, "w")
+    for line in 1:size(inputs,1)
+        string_line=""
+        for item in inputs[line,:]
+            string_line=string(string_line,item,",")
+        end
+        if (targets[line])
+            string_line=string(string_line,"1")
+        else
+            string_line=string(string_line,"0")
+        end
+        print(f,string_line) 
+        println(f,"")
+    end
 end
 
 # =============================================================================
 # Nuestro codigo usara:
-(inputs, targets) = getInputs("../aa_2021");
+(inputs, targets) = getInputs("datasets");
---
+++ practica_rna.jl
@@ -0,0 +1,49 @@
+include("modulos/bondad.jl")
+include("modulos/graphics.jl")
+include("modulos/datasets.jl")
+include("modulos/rna.jl")
+
+
+#RECORDAR:
+#   Para revisar que esté todo bien mostrar gráficos del codigo del 
+#   profe y el nuestro, conjunto test tintinea un poco
+
+# Parametros principales de la RNA y del proceso de entrenamiento
+topology = [4, 3]; # Dos capas ocultas con 4 neuronas la primera y 3 la segunda
+learningRate = 0.01; # Tasa de aprendizaje
+numMaxEpochs = 1000; # Numero maximo de ciclos de entrenamiento
+validationRatio = 0.2; # Porcentaje de patrones que se usaran para validacion
+testRatio = 0.2; # Porcentaje de patrones que se usaran para test
+maxEpochsVal = 6; # Numero de ciclos en los que si no se mejora el loss en el conjunto de validacion, se para el entrenamiento
+
+# Cargamos el dataset
+dataset = readdlm("datasets/iris.data",',');
+# Preparamos las entradas y las salidas deseadas
+inputs = convert(Array{Float64,2}, dataset[:,1:4]);
+targets = oneHotEncoding(dataset[:,5]);
+
+# Normalizamos las entradas, a pesar de que algunas se vayan a utilizar para test
+normalizeMinMax!(inputs);
+
+# Creamos los indices de entrenamiento, validacion y test
+(trainingIndices, validationIndices, testIndices) = holdOut(size(inputs,1), validationRatio, testRatio);
+
+# Dividimos los datos
+trainingInputs    = inputs[trainingIndices,:];
+validationInputs  = inputs[validationIndices,:];
+testInputs        = inputs[testIndices,:];
+trainingTargets   = targets[trainingIndices,:];
+validationTargets = targets[validationIndices,:];
+testTargets       = targets[testIndices,:];
+
+# Y creamos y entrenamos la RNA con los parametros dados
+(ann, trainingLosses, validationLosses, testLosses, trainingAccuracies,
+validationAccuracies, testAccuracies) = trainClassANN(topology, trainingInputs, trainingTargets, validationInputs,
+                                            validationTargets, testInputs, testTargets; maxEpochs=numMaxEpochs,
+                                            learningRate=learningRate, maxEpochsVal=maxEpochsVal, showText=true);
+
+print_train_results(trainingLosses, validationLosses, testLosses, trainingAccuracies, validationAccuracies, testAccuracies) 
+
+#Resultados finales sobre todos los patrones:
+accuracy(Array{Float64,2}(ann(inputs')'),targets)
+confusionMatrix(float_outputs_to_bool(Array{Float64,2}(ann(inputs')'),true),targets,true)  
