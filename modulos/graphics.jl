0389af7156236474fd7acc327152cdfa21af56d6
the-scripting-boy <hectorpadint00@gmail.com>
2021-03-18 19:14:18 +0100

    Improved gpu function.

--- modulos/graphics.jl
+++ modulos/graphics.jl
@@ -1,6 +1,6 @@
 # =============================================================================
 # graphics.jl -> Funciones útiles para visualizar gráficamente los resultados de una RNA:
-#   - 
+#   -
 # =============================================================================
 
 using Plots
@@ -9,11 +9,11 @@ function print_train_results(
     trainingLosses::Array{Float64,1},validationLosses::Array{Float64,1},testLosses::Array{Float64,1},
     trainingAccuracies::Array{Float64,1},validationAccuracies::Array{Float64,1},testAccuracies::Array{Float64,1})
     x=1:size(trainingLosses,1)
-    losses=plot(x,[trainingLosses validationLosses testLosses],title = "Loss",label = ["Training" "Validation" "Test"],)
-    xlabel!("Epoch")
-    ylabel!("Error (CrossEntropy (Creo))")
-    accuracy=plot(x,[trainingAccuracies validationAccuracies testAccuracies],title = "Accuracies",label = ["Training" "Validation" "Test"],)
-    xlabel!("Epoch")
-    ylabel!("% Accuracies")
-    display(plot(losses,accuracy))
+    losses=plot(x,[trainingLosses validationLosses testLosses],title = "Loss",label = ["Training" "Validation" "Test"],);
+    xlabel!("Epoch");
+    ylabel!("Error (CrossEntropy (Creo))");
+    accuracy=plot(x,[trainingAccuracies validationAccuracies testAccuracies],title = "Accuracies",label = ["Training" "Validation" "Test"],);
+    xlabel!("Epoch");
+    ylabel!("% Accuracies");
+    display(plot(losses,accuracy));
 end
