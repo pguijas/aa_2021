72aabb29873179926591dee42c667020ae793ae7
the-scripting-boy <hectorpadint00@gmail.com>
2021-04-16 14:19:14 +0200

    data visualization + loop de svm y knn.

--- loop_svm.jl
+++ loop_svm.jl
@@ -1,7 +1,6 @@
 include("modulos/datasets.jl")
 include("modulos/attributes_from_dataset.jl")
 include("modulos/models_cross_validation.jl")
-using Flux
 
 seed!(1);
 
@@ -22,15 +21,61 @@ targets = convert(Array{Any,1},dataset[:,7]);                   #Array{Bool,2}
 numFolds = 10;
 
 # Parametros del SVM
-kernel = "rbf"; #linear, poly, rbf, sigmoid, precomputed
+kernel = "linear"; #linear, poly, rbf, sigmoid, precomputed
 kernelDegree = 3; # en caso de que sea 'poly', indica el grado del polinomio (num imputs)
 kernelGamma = 2; #coeficiente del kernel para rbf, poly y sigmoid
 C=1; #parámetro de regularización
 
-# Entrenamos las SVM
 modelHyperparameters = Dict();
 modelHyperparameters["kernel"] = kernel;
 modelHyperparameters["kernelDegree"] = kernelDegree;
 modelHyperparameters["kernelGamma"] = kernelGamma;
 modelHyperparameters["C"] = C;
 modelCrossValidation(:SVM, modelHyperparameters, inputs, targets, numFolds);
+
+max_σ = 25:
+mean_acc = [];
+sdev = [];
+for kernelGamma in 2:max_σ
+    kernel = "rbf";
+    modelHyperparameters = Dict();
+    modelHyperparameters["kernel"] = kernel;
+    modelHyperparameters["kernelDegree"] = kernelDegree;
+    modelHyperparameters["kernelGamma"] = kernelGamma;
+    modelHyperparameters["C"] = C;
+    (testAccuracies, testStd, _, _) = modelCrossValidation(:SVM, modelHyperparameters, inputs, targets, numFolds);
+    push!(mean_acc,testAccuracies);
+    push!(sdev,testStd);
+end;
+
+m = plot([2:max_σ],mean_acc,title = "Accurracies",label = "Accurracy",);
+xlabel!("σ");
+ylabel!("Precision");
+stdd = plot([2:max_σ],sdev,title = "Standard Deviation",label = "std",);
+xlabel!("σ");
+ylabel!("%");
+display(plot(m,stdd));
+
+
+mean_acc = [];
+sdev = [];
+kernelDegree = 5;
+for kernelGamma in 2:max_σ
+    kernel = "poly";
+    modelHyperparameters = Dict();
+    modelHyperparameters["kernel"] = kernel;
+    modelHyperparameters["kernelDegree"] = kernelDegree;
+    modelHyperparameters["kernelGamma"] = kernelGamma;
+    modelHyperparameters["C"] = C;
+    (testAccuracies, testStd, _, _) = modelCrossValidation(:SVM, modelHyperparameters, inputs, targets, numFolds);
+    push!(mean_acc,testAccuracies);
+    push!(sdev,testStd);
+end;
+
+m = plot([2:max_σ],mean_acc,title = "Accurracies",label = "Accurracy",);
+xlabel!("σ");
+ylabel!("Precision");
+stdd = plot([2:max_σ],sdev,title = "Standard Deviation",label = "std",);
+xlabel!("σ");
+ylabel!("%");
+display(plot(m,stdd));
