af9d651a0b87493399659fa11b40032181ac1bc2
the-scripting-boy <hectorpadint00@gmail.com>
2021-05-10 19:09:34 +0200

    mirar arrays.

---
+++ DL.jl
@@ -0,0 +1,113 @@
+using Flux
+using Flux: onehotbatch, onecold, crossentropy
+using JLD2, FileIO
+using Statistics: mean
+
+include("modulos/dataset_DL.jl")
+
+interval = 6;
+(train_imgs, train_labels,
+    test_imgs, test_labels) = getInputs("datasets", interval);
+
+
+println("Tamaño de la matriz de entrenamiento: ", size(train_imgs))
+println("Tamaño de la matriz de test:          ", size(test_imgs))
+
+batch_size = 128;
+gruposIndicesBatch = Iterators.partition(1:size(train_imgs,1), batch_size);
+println("He creado ", length(gruposIndicesBatch), " grupos de indices para distribuir los patrones en batches");
+
+train_set = [( train_imgs[indicesBatch], Array(onehotbatch(train_labels[indicesBatch], 0:9)) ) for indicesBatch in gruposIndicesBatch]
+
+test_set = (test_imgs, onehotbatch(test_labels, 0:9));
+
+train_imgs = nothing;
+test_imgs = nothing;
+GC.gc();
+
+funcionTransferenciaCapasConvolucionales = relu;
+
+# Definimos la red con la funcion Chain, que concatena distintas capas
+modelo = Chain(
+    Conv((3, 3), 1=>16, pad=(1,1), funcionTransferenciaCapasConvolucionales),
+    MaxPool((2,2)),
+    Conv((3, 3), 16=>32, pad=(1,1), funcionTransferenciaCapasConvolucionales),
+    MaxPool((2,2)),
+    Conv((3, 3), 32=>32, pad=(1,1), funcionTransferenciaCapasConvolucionales),
+    MaxPool((2,2)),
+    x -> reshape(x, :, size(x, 4)),
+    Dense(288, 10),
+    softmax
+);
+
+
+# Vamos a probar la RNA y poner algunos datos de cada capa
+# Usaremos como entrada varios patrones de un batch
+numBatchCoger = 1; numImagenEnEseBatch = [12, 6];
+
+
+ntradaCapa = train_set[numBatchCoger][1][:,:,:,numImagenEnEseBatch];
+numCapas = length(params(modelo));
+println("La RNA tiene ", numCapas, " capas:");
+for numCapa in 1:numCapas
+    println("   Capa ", numCapa, ": ", modelo[numCapa]);
+    # Le pasamos la entrada a esta capa
+    global entradaCapa # Esta linea es necesaria porque la variable entradaCapa es global y se modifica en este bucle
+    capa = modelo[numCapa];
+    salidaCapa = capa(entradaCapa);
+    println("      La salida de esta capa tiene dimension ", size(salidaCapa));
+    entradaCapa = salidaCapa;
+end
+
+modelo(train_set[numBatchCoger][1][:,:,:,numImagenEnEseBatch]);
+
+loss(x, y) = crossentropy(modelo(x), y)
+accuracy(batch) = mean(onecold(modelo(batch[1])) .== onecold(batch[2]))
+
+println("Ciclo 0: Precision en el conjunto de entrenamiento: ", 100*mean(accuracy.(train_set)), " %");
+
+pt = ADAM(0.001);
+
+
+println("Comenzando entrenamiento...")
+mejorPrecision = -Inf;
+criterioFin = false;
+numCiclo = 0;
+numCicloUltimaMejora = 0;
+mejorModelo = nothing;
+
+while (!criterioFin)
+
+    global numCicloUltimaMejora, numCiclo, mejorPrecision, mejorModelo;
+
+    Flux.train!(loss, params(modelo), train_set, opt);
+
+    numCiclo += 1;
+
+    precisionEntrenamiento = mean(accuracy.(train_set));
+    println("Ciclo ", numCiclo, ": Precision en el conjunto de entrenamiento: ", 100*precisionEntrenamiento, " %");
+
+    if (precisionEntrenamiento >= mejorPrecision)
+        mejorPrecision = precisionEntrenamiento;
+        precisionTest = accuracy(test_set);
+        println("   Mejora en el conjunto de entrenamiento -> Precision en el conjunto de test: ", 100*precisionTest, " %");
+        mejorModelo = deepcopy(modelo);
+        numCicloUltimaMejora = numCiclo;
+    end;
+
+    if (numCiclo - numCicloUltimaMejora >= 5) && (opt.eta > 1e-6)
+        opt.eta /= 10.0
+        println("   No se ha mejorado en 5 ciclos, se baja la tasa de aprendizaje a ", opt.eta);
+        numCicloUltimaMejora = numCiclo;
+    end;
+
+    if (precisionEntrenamiento >= 0.999)
+        println("   Se para el entenamiento por haber llegado a una precision de 99.9%")
+        criterioFin = true;
+    end;
+
+    if (numCiclo - numCicloUltimaMejora >= 10)
+        println("   Se para el entrenamiento por no haber mejorado la precision en el conjunto de entrenamiento durante 10 ciclos")
+        criterioFin = true;
+    end;
+end;
--- modulos/dataset_DL.jl
+++ modulos/dataset_DL.jl
@@ -3,7 +3,7 @@ using FileIO;
 
 
 function imageToArray(image::Array{RGB{Normed{UInt8,8}},2})
-    matrix = Array{Float32, 3}(undef, size(image,1), size(image,2), 3)
+    matrix = Array{Float32, 4}(undef, size(image,1), size(image,2), 3, 1)
     matrix[:,:,1] = convert(Array{Float32,2}, red.(image));
     matrix[:,:,2] = convert(Array{Float32,2}, green.(image));
     matrix[:,:,3] = convert(Array{Float32,2}, blue.(image));
@@ -25,13 +25,15 @@ function loadFolderImages(folderName::String, testRatio::Int64)
     for fileName in readdir(folderName)
         if isImageExtension(fileName)
             # Leemos la imagen
+            print(fileName)
             image = load(string(folderName, "/", fileName));
             # Comprobar que el archivo cargado sea una imagen en color
             if (typeof(image)==Array{RGBX{Normed{UInt8,8}},2})
                 image=convert(Array{RGB{Normed{UInt8,8}},2},image);
             end;
             @assert(isa(image, Array{RGBA{Normed{UInt8,8}},2}) || isa(image, Array{RGB{Normed{UInt8,8}},2}))
-
+            @show(size(image))
+            @assert (size(image)==(150,150)) "Las imagenes no tienen tamaño 150x150";
             if (mod(count,testRatio)==0)
                 # Añadimos la foto al array de test
                 push!(testImages, image);
@@ -70,25 +72,25 @@ function getInputs(path::String, testRatio::Int64)
     sizeNegativeDatasetTest = size(testNegativo,1);
     testLength = sizeFaceDatasetTest + sizeMaskDatasetTest + sizeNegativeDatasetTest;
 
-    #train_imgs = Array{Float32, 4}(undef, size(image,1), size(image,2), 3, length);
-    train_imgs = [];
+    train_imgs = Array{Float32, 4}(undef, 150, 150, 3, length);
+    #train_imgs = [];
     train_labels = Array{Int64, 1}(undef, length);
-    #test_imgs = Array{Float32, 4}(undef, size(image,1), size(image,2), 3, testLength);
-    test_imgs = [];
+    test_imgs = Array{Float32, 4}(undef, 150, 150, 3, testLength);
+    #test_imgs = [];
     test_labels = Array{Int64, 1}(undef, testLength);
 
     # Generamos la primera parte de la matriz de inputs con los elementos
     # que son positivos
     for i in 1:sizeFaceDataset
         train_labels[i] = 0;
-        push!(train_imgs, caras[i]);
+        train_imgs[i] = caras[i];
     end;
 
     # Generamos la tercera parte de la matriz de inputs con los elementos
     # que son caras con mascarilla
     for i in 1:sizeMaskDataset
         train_labels[sizeFaceDataset+i] = 1;
-        push!(train_imgs, mascarillas[i]);
+        train_imgs[sizeFaceDataset+i] = mascarillas[i];
     end;
 
     # Generamos la segunda parte de la matriz de inputs con los elementos
@@ -96,21 +98,21 @@ function getInputs(path::String, testRatio::Int64)
     aux = sizeFaceDataset+sizeMaskDataset;
     for i in 1:sizeNegativeDataset
         train_labels[aux+i] = 2;
-        push!(train_imgs, negativo[i]);
+        train_imgs[aux+i] = negativo[i];
     end;
 
     # Generamos la primera parte de la matriz de inputs con los elementos
     # que son positivos
     for i in 1:sizeFaceDatasetTest
         test_labels[i] = 0;
-        push!(test_imgs, testCaras[i]);
+        test_imgs[i] = testCaras[i];
     end;
 
     # Generamos la tercera parte de la matriz de inputs con los elementos
     # que son caras con mascarilla
     for i in 1:sizeMaskDatasetTest
         test_labels[sizeFaceDatasetTest+i] = 1;
-        push!(test_imgs, testMascarillas[i]);
+        test_imgs[sizeFaceDatasetTest+i] = testMascarillas[i];
     end;
 
     # Generamos la segunda parte de la matriz de inputs con los elementos
@@ -118,13 +120,8 @@ function getInputs(path::String, testRatio::Int64)
     aux = sizeFaceDatasetTest+sizeMaskDatasetTest;
     for i in 1:sizeNegativeDatasetTest
         test_labels[aux+i] = 2;
-        push!(test_imgs, testNegativo[i]);
+        test_imgs[aux+i] = testNegativo[i];
     end;
 
     return (train_imgs, train_labels, test_imgs, test_labels);
-end
-
-
-(train_imgs, train_labels, test_imgs, test_labels) = getInputs("datasets",6);
-@assert(size(train_imgs,1)==size(train_labels,1))
-@assert(size(test_imgs,1)==size(test_labels,1))
+end;
